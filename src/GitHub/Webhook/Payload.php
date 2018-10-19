118c134c07fc472011c8fa7c0bddbfd970c098db
David Beuchert <mail@dbeuchert.com>
2018-10-19 17:55:56 +0200

    adds subPayload functionality

--- src/GitHub/Webhook/Payload.php
+++ src/GitHub/Webhook/Payload.php
@@ -39,4 +39,59 @@
             return $this->payload;
         }
 
+        /**
+         * @param string $key
+         *
+         * @return bool
+         */
+        function hasSubPayload (string $key) : bool {
+            return isset($this->getPayloadAsArray()[$key]);
+        }
+
+        /**
+         * @param string $key
+         *
+         * @return null|Payload
+         */
+        function getSubPayload (string $key) : ?Payload {
+            if (!$this->hasSubPayload($key)) {
+                return null;
+            }
+
+            return $this->createSubPayloadFromData($this->getPayloadAsArray()[$key]);
+        }
+
+        /**
+         * @param string $key
+         *
+         * @return \Iterator
+         */
+        function getSubPayloadsIterator (string $key) : \Iterator {
+            if (!$this->hasSubPayload($key)) {
+                return new \Generator();
+            }
+
+            $subPayloadData = $this->getPayloadAsArray()[$key];
+            if (!is_array($subPayloadData)) {
+                $subPayloadData = [$subPayloadData];
+            }
+
+            foreach ($subPayloadData as $subPayload) {
+                yield $this->createSubPayloadFromData($subPayload);
+            }
+        }
+
+        /**
+         * @param array $subPayloadData
+         *
+         * @return Payload
+         */
+        private function createSubPayloadFromData (array $subPayloadData) : Payload {
+            $subPayload = clone $this;
+            $subPayload->payload = $subPayloadData;
+            $subPayload->jsonPayload = json_encode($subPayload->payload, JSON_PRETTY_PRINT);
+
+            return $subPayload;
+        }
+
     }
