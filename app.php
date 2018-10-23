13aa24870b6316f42cb5931d230a441a0db9409f
David Beuchert <mail@dbeuchert.com>
2018-10-23 10:50:11 +0200

    use Slimmer to manage HTTP requests and responses

--- app.php
+++ app.php
@@ -2,26 +2,40 @@
 
     require_once 'vendor/autoload.php';
 
-    $webhook = \Tholabs\ContinousStaging\GitHub\Webhook\WebhookRequest::createFromRequest();
-    $event = $webhook->getEvent();
+    $app = new \Slimmer\App();
 
-    if ($event instanceof \Tholabs\ContinousStaging\GitHub\Events\PushEvent) {
-        $refs = explode('/', $event->getHeader()->getRef());
-        $branch = array_pop($refs);
-        $project = $event->getRepository()->getName();
-        $user = $event->getRepository()->getOwner();
+    $app->getRouter()->registerPostHandler(null, function () {
 
-        $targetPath = implode(DIRECTORY_SEPARATOR, [__DIR__, 'projects', $project, $user->getName(), $branch]);
         try {
-            $repository = new \Cz\Git\GitRepository($targetPath);
-            $repository->pull();
-        } catch (\Cz\Git\GitException $exception) {
+            $webhook = \Tholabs\ContinousStaging\GitHub\Webhook\WebhookRequest::createFromRequest();
+        } catch (\Tholabs\ContinousStaging\Exceptions\BadWebhookRequest $badWebhook) {
+            throw (new \Slimmer\Exceptions\Http\BadRequest($badWebhook->getMessage()))
+                ->setExceptionOutput([
+                    'message' => $badWebhook->getMessage()
+                ]);
+        }
+
+        $event = $webhook->getEvent();
+
+        if ($event instanceof \Tholabs\ContinousStaging\GitHub\Events\PushEvent) {
+            $refs = explode('/', $event->getHeader()->getRef());
+            $branch = array_pop($refs);
+            $project = $event->getRepository()->getName();
+            $user = $event->getRepository()->getOwner();
+
+            $targetPath = implode(DIRECTORY_SEPARATOR, [__DIR__, 'projects', $project, $user->getName(), $branch]);
             try {
-                $repository = \Cz\Git\GitRepository::cloneRepository($event->getRepository()->getCloneUrls()->getHttpWithUsernameAndPassword('somebody', 'somewhere'), $targetPath);
+                $repository = new \Cz\Git\GitRepository($targetPath);
+                $repository->pull();
             } catch (\Cz\Git\GitException $exception) {
-                echo "error: {$exception->getMessage()}";
+                try {
+                    $repository = \Cz\Git\GitRepository::cloneRepository($event->getRepository()->getCloneUrls()
+                        ->getHttpWithUsernameAndPassword('somebody', 'somewhere'), $targetPath);
+                } catch (\Cz\Git\GitException $exception) {
+                    echo "error: {$exception->getMessage()}";
+                }
             }
         }
-    }
+    });
 
-    echo "\nfinish";
+    $app->run();
